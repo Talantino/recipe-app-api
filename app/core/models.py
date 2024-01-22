01509f8315bdd8986d1517e7229eec00758197ab
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-01-22 20:57:27 +0600

    Configured and customized django admin list users

--- app/core/models.py
+++ app/core/models.py
@@ -2,11 +2,12 @@
 Database models.
 """
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -15,6 +16,7 @@ class UserManager(BaseUserManager):
         """Create, save and return a new user"""
         if not email:
             raise ValueError('User must have an email address')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
